3aba548b0c45c428632a8629fce6bc69f473dcf5
Rajakgm <30368233+Rajakgm@users.noreply.github.com>
2017-08-22 16:02:47 +0530

    Create then.c

---
+++ then.c
@@ -0,0 +1,15 @@
+# kavi
+#include <stdio.h>
+int main()
+{
+    char t;
+    printf("Enter a character: ");
+    scanf("%c",&t);
+
+    if( (t>='a' && c<='z') || (t>='A' && t<='Z'))
+        printf("%c is an alphabet.",t);
+    else
+        printf("%c is not an alphabet.",t);
+
+    return 0;
+}
