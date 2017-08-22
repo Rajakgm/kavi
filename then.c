e4795d544e0b09ee8abffb1103bd16920e3ad166
Rajakgm <30368233+Rajakgm@users.noreply.github.com>
2017-08-22 16:19:01 +0530

    Update then.c

--- then.c
+++ then.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter a character: ");
     scanf("%c",&t);
 
-    if( (t>='a' && c<='z') || (t>='A' && t<='Z'))
+    if( (t>='a' && t<='z') || (t>='A' && t<='Z'))
         printf("%c is an alphabet.",t);
     else
         printf("%c is not an alphabet.",t);
